779e1f23689b3f48674db86c95823d91a8b45be1
Josefin Törner <josefintorner@gmail.com>
2024-10-31 15:33:52 +0100

    Changed in main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
 
-    public Main(){
+    public static void main(String[] args) {
         GameFrame frame = new GameFrame();
     }
 
-    public static void main(String[] args) {
-        Main main = new Main();
-    }
 }
